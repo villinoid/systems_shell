89f49973a2e86f0aaa0d27849a6ad70b95dccc70
fsiniscalco10@stuy.edu <fsiniscalco10@stuy.edu>
2020-12-13 02:10:30 +0100

    updated

--- main.c
+++ main.c
@@ -9,12 +9,31 @@ int main(){
 	char input_buffer[1000];
 	char** args;
 	int i;
+	int finished;
 	while(1){
 		fgets(input_buffer,sizeof(input_buffer),stdin);
 		printf("%s",input_buffer);
 		args=parse_args(input_buffer);
-		execvp(args[0],args);
+		//Check what type it is
+		//exit
+		if(!strcmp(args[0],"exit"))
+			return 0;
+		//Redirct
+
+		//Pipe
+
+		//Normal run ls or whatever
+		int f=0;
+		f=fork();
+		if(!f){
+			finished=0;
+			finished=execvp(args[0],args);//ERRNO????//ERRNO????//ERRNO????//ERRNO????//ERRNO????//ERRNO????
+			printf("CHILD PROCESS - ERROR WITH COMMAND INPUT\n");
+			return 1;
+			}
+		else
+			free(args);
 	}
-	return 0;q
+	return 0;
 }
 		
